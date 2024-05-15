86afd0667c4c6ef1d5a7f6aa14347add0cbeea4d
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2024-05-15 22:11:34 +0200

    add helper function to format numbers with commas

--- main.js
+++ main.js
@@ -22,16 +22,17 @@ class Calculator {
         // elements that need to be updated during the calculation
         this.previousOperandText = previousOperandText;
         this.currentOperandText = currentOperandText;
+
         // Clear the calculator when it is initialized
+        // Set key values to empty or undefined
         this.clear();
+
         // Bind events to the buttons
         this.bindEvents();
     }
 
     clear() {
-        console.log('HELLO!');
-        console.log(previousOperandText);
-        console.log(currentOperandText);
+        // These values are defined on initialization and are updated during the calculation
         this.currentOperand = '';
         this.previousOperand = '';
         this.operation = undefined; // No operation selected
@@ -110,13 +111,28 @@ class Calculator {
         this.previousOperand = '';
     }
 
-    updateDisplay() {
-        this.currentOperandText.innerText = this.currentOperand;
-        this.previousOperandText.innerText = this.previousOperand;
+    // Format the display of the number to use commas
+    formatDisplay(number) {
+        // Convert from a string to a number
+        const floatNumber = parseFloat(number);
+        // If the number is NaN, return an empty string
+        // because the number cannot be formatted
+        if (isNaN(floatNumber)) return '';
+        // Return the number formatted with commas
+        return floatNumber.toLocaleString('en');
+    }
 
-        // If an operation is selected, display the previous operand and the operation
+    updateDisplay() {
+        // A - Update the display of the current operand
+        this.currentOperandText.innerText = this.formatDisplay(
+            this.currentOperand,
+        );
+
+        // B - Update the display of the previous operand
+        // If an operation is selected
         if (this.operation != null) {
-            this.previousOperandText.innerText = `${this.previousOperand} ${this.operation}`;
+            // Display the previous operand and the operation in the previous operand text
+            this.previousOperandText.innerText = `${this.formatDisplay(this.previousOperand)} ${this.operation}`;
         }
     }
 
@@ -152,147 +168,8 @@ class Calculator {
     }
 }
 
-/*
-class Calculator {
-    constructor() {
-        // Initialize calculator properties
-        // The result of the calculation
-        this.result = 0;
-        // The value displayed on the calculator
-        this.displayValue = '';
-        // The current operator
-        this.currentOperator = null;
-        // The value to be used in the next calculation
-        this.pendingValue = null;
-        // Whether to reset the display on the next input
-        this.shouldResetDisplay = false;
-        // Bind events to the buttons
-        this.bindEvents();
-    }
-
-    bindEvents() {
-        // Numeric buttons
-        numericButtons.forEach(button => {
-            button.addEventListener('click', event =>
-                this.handleNumericInput(event),
-            );
-        });
-        // Operator buttons
-        operatorButtons.forEach(button => {
-            button.addEventListener('click', event =>
-                this.handleOperatorInput(event),
-            );
-        });
-    }
-
-    // 01 - Handle numeric input
-    handleNumericInput(event) {
-        // Get the number from the data attribute
-        const dataNum = event.target.dataset.num;
-
-        // If the shouldResetDisplay flag is true,
-        // it means an operator was just pressed,
-        // and we should start fresh with the new number
-        if (this.shouldResetDisplay) {
-            // Reset display with the new number
-            this.displayValue = dataNum;
-            // Reset the flag
-            this.shouldResetDisplay = false;
-        } else {
-            // else, append the number to the current display value
-            this.displayValue += dataNum;
-        }
-        // Update the display
-        this.display();
-    }
-
-    // 02 - Handle operator input
-    handleOperatorInput(event) {
-        // Get the operator from the data attribute
-        const operator = event.target.dataset.op;
-        // Check if the operator is the equals sign
-        if (operator === 'eq') {
-            // If it is, calculate the result
-            this.calculate();
-            // Update the display
-            this.display();
-        } else {
-            // If any other operator was pressed
-            if (this.currentOperator) {
-                // If there's already a current operator stored
-                // it means we are chaining operations,
-                // so call calculate() to compute the result so far.
-                this.calculate();
-            } else {
-                // flag is false, the new number is appended to the existing displayValue,
-                // allowing multi-digit numbers to be formed.
-                this.result = Number(this.displayValue);
-            }
-            // Set the current operator
-            this.currentOperator = operator;
-            // Set the pending value to the current display value
-            this.pendingValue = Number(this.displayValue);
-            // Set the flag to reset the display
-            this.shouldResetDisplay = true;
-        }
-    }
-
-    // 03 - Display the value
-    display() {
-        calculatorDisplay.innerText = this.displayValue;
-    }
-
-    // 04 - Clear the display
-    clearDisplay() {
-        this.displayValue = '';
-        this.display();
-    }
-
-    // 05 - Calculate the result
-    calculate() {
-        const currentValue = Number(this.displayValue);
-        switch (this.currentOperator) {
-            case 'add':
-                this.result += currentValue;
-                break;
-            case 'sub':
-                this.result -= currentValue;
-                break;
-            case 'mul':
-                this.result *= currentValue;
-                break;
-            case 'div':
-                if (currentValue === 0) {
-                    alert('Cannot divide by zero');
-                    return;
-                }
-                this.result /= currentValue;
-                break;
-            case 'clear':
-                this.clear();
-                return;
-            default:
-                return;
-        }
-        // Update the display with the result as a string
-        this.displayValue = this.result.toString();
-        // Reset the flag
-        this.currentOperator = null;
-    }
-
-    // 06 - Clear the calculator
-    clear() {
-        this.result = 0;
-        this.displayValue = '';
-        this.currentOperator = null;
-        this.pendingValue = null;
-        this.shouldResetDisplay = false;
-        this.display();
-    }
-}
-*/
 // -----------------------------------------------------------------------------
-// INITIALIZATION
+// INITIALIZE CALCULATOR
 // -----------------------------------------------------------------------------
 
 document.addEventListener('DOMContentLoaded', () => {
